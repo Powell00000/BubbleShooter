5b4737535ce7236d8ad856de0bfcbe0eed42e583
Paweł Miłaszewicz <pawelm@kool2play.com>
2021-02-10 17:15:22 +0100

    Shoot line code

--- BubbleShooter/Assets/Code/Mono/Cannon.cs
+++ BubbleShooter/Assets/Code/Mono/Cannon.cs
@@ -1,5 +1,8 @@
 using Assets.Code.Bubbles.Mono;
 using Assets.Code.Mono;
+using BansheeGz.BGSpline.Curve;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Cannon : MonoBehaviour
@@ -12,23 +15,103 @@ public class Cannon : MonoBehaviour
 
     [Zenject.Inject]
     private CameraBounds cameraBounds;
+
     private ShootingBubble shootingBubblePrefab;
 
     [SerializeField]
-    private BansheeGz.BGSpline.Curve.BGCurve shootCurve;
+    private BGCurve shootCurve;
 
     private void Start()
     {
+        StartCoroutine(ShootLineRoutine());
     }
 
     private void Update()
     {
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 position = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, 0);
+        if (Input.GetMouseButtonUp(0))
+        {
 
-        Vector3 forwardLook = (position - pivot.position).normalized;
+        }
+    }
 
-        pivot.rotation = Quaternion.LookRotation(Vector3.forward, forwardLook);
+    private IEnumerator ShootLineRoutine()
+    {
+        while (true)
+        {
+            Vector3 mouseWorldPosition = cameraBounds.Cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, 0);
 
+            Vector3 normalToTarget = (position - pivot.position).normalized;
+
+            List<Vector3> points = new List<Vector3>();
+
+            Quaternion desiredRotation = Quaternion.LookRotation(Vector3.forward, normalToTarget);
+            float zAngle = Vector3.SignedAngle(Vector3.up, normalToTarget, Vector3.forward);
+            zAngle = Mathf.Clamp(zAngle, -60, 60);
+            Quaternion clampedRotation = Quaternion.Euler(desiredRotation.eulerAngles.x, desiredRotation.eulerAngles.y, zAngle);
+            pivot.rotation = clampedRotation;
+
+            Vector3 lookNormal = clampedRotation * Vector3.up;
+
+            Vector3 rayForward = lookNormal;
+            Vector3 rayStartPosition = pivot.position;
+
+            Debug.DrawRay(rayStartPosition, rayForward * 20, Color.red);
+
+            points.Add(pivot.position);
+
+            yield return null;
+
+            bool exitLoop = false;
+            while (!exitLoop)
+            {
+                Ray ray = new Ray()
+                {
+                    direction = rayForward,
+                    origin = rayStartPosition
+                };
+
+                if (Physics.Raycast(ray, out var raycastHit))
+                {
+                    rayForward = Vector3.Reflect(ray.direction, raycastHit.normal);
+                    rayStartPosition = raycastHit.point;
+
+                    Debug.DrawRay(rayStartPosition, rayForward * 20, Color.red);
+                    points.Add(raycastHit.point);
+
+                    if (raycastHit.rigidbody.gameObject.name == "TopWall")
+                    {
+                        exitLoop = true;
+                    }
+                }
+            }
+
+            if (shootCurve.PointsCount > points.Count)
+            {
+                shootCurve.Clear();
+            }
+
+            if (shootCurve.PointsCount < points.Count)
+            {
+                shootCurve.Clear();
+            }
+
+            if (shootCurve.PointsCount == points.Count)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    shootCurve.Points[i].PositionWorld = points[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    shootCurve.AddPoint(shootCurve.CreatePointFromWorldPosition(points[i], BGCurvePoint.ControlTypeEnum.Absent));
+                }
+            }
+
+            yield return null;
+        }
     }
 }
