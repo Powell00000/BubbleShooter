8b41be45cd6d53bafb5611216ad7726b0c234ff7
Paweł Miłaszewicz <pawelm@kool2play.com>
2021-02-10 11:37:27 +0100

    randomizing initial numbers and updating them

---
+++ BubbleShooter/Assets/Code/DOTS/UnmarAsSpawnedSystem.cs
@@ -0,0 +1,22 @@
+﻿
+using Unity.Entities;
+
+namespace Assets.Code.DOTS
+{
+    internal class UnmarAsSpawnedSystem : SystemBaseWithBarriers
+    {
+        protected override void OnUpdate()
+        {
+            var beginInitBuffer = beginInitializationBuffer.CreateCommandBuffer();
+            Entities
+                .WithAll<JustSpawnedTagCmp>()
+                .ForEach((Entity e) =>
+                {
+                    beginInitBuffer.RemoveComponent<JustSpawnedTagCmp>(e);
+                })
+                .Schedule();
+
+            beginInitializationBuffer.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
